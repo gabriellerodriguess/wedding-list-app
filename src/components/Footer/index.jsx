11a7342a96e8e6a6b3df9a1e249f9859b7d94dd5
Gabrielle <gabrielle@MacBook-de-Gabrielle.local>
2024-08-07 20:34:25 -0300

    fix: favicon href

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -15,14 +15,14 @@ export default function Footer() {
                 <p>O evento será no dia <strong>08/09/2024</strong> às <strong>11h</strong></p>
                 <p>No endereço: <strong>QS 116 CONJUNTO 6 LOTE 5 | Samambaia</strong></p>
             </div>
-            <div className='container_footer'>
+            {/* <div className='container_footer'>
                 <div className='container_footer-text'>
                     <h1>Faltam dias</h1>
                 </div>
                 <div className='container_footer-date'>
                     <h2>{createDate()}</h2>
                 </div>
-            </div>
+            </div> */}
         </footer>
     )
 }
